9f019584e94bc69070b598aa7813af2026f8a114
Anurag607 <maskeddeep@gmail.com>
2024-03-03 21:38:23 +0530

    fix: fixed root

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 import { ReduxProviders } from './redux/reduxProvider.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('ecowiser-verified')).render(
   <React.StrictMode>
     <ReduxProviders>
       <App />
