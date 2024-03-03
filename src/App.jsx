8c745f26ae4b10c527528805d0cafa3226dbf8b6
Anurag607 <maskeddeep@gmail.com>
2024-03-03 21:51:00 +0530

    fix: added portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,13 @@
-import { useState, useEffect } from 'react';
-import OffCanvasPopup from "./Popup/OffCanvasPopup.jsx";
-import VerfiedContainer from "./VerfiedContainer/index.jsx";
-import { badgeDetails } from './helper.mjs';  
-import { useDispatch } from 'react-redux';
-import { setAllBadges, setBrandUrl } from './redux/drawerSlice.mjs';
+import Widget from './Widget';
+import { createPortal } from 'react-dom';
 
-function App() {
-  const dispatch = useDispatch();
-  const [url, setUrl] = useState('');
-  const [badges, setBadges] = useState([]);
-
-  useEffect(() => {
-    const ecowiserVerified = document.querySelector("#ecowiser-verified");
-    const url = ecowiserVerified.dataset.url;
-    let tags = ecowiserVerified.dataset.badges.replace("Low Carbon", "Offsets Carbon").split(', ');
-    tags = tags.map((el, _) => { return el.toLowerCase(); });
-    let badges = [];
-    badgeDetails.forEach((category, _) => {
-      category.badges.forEach((badge, _) => {
-        if(tags.includes(badge.name.toLowerCase())) badges.push(badge);
-      })
-    });
-
-    setUrl(url);
-    setBadges(badges);
-    dispatch(setAllBadges(badges));
-    dispatch(setBrandUrl(url));
-  }, []);
-
+const App = () => {
   return (
-    <>
-      <OffCanvasPopup />
-      <VerfiedContainer tags={badges} url={url} />
-    </>
+    createPortal(
+      <Widget />,
+      document.getElementById('ecowiser-verified')
+    )
   )
 }
 
-const SampleWrapper = ({children}) => {
-  return (
-    <div className={"relative rubik w-screen h-screen flex flex-col items-center justify-center"}>
-      <div className={"relative mobile:w-[95%] w-[28rem] h-fit grid place-items-center"}>
-          {children}
-      </div>
-    </div>
-  )
-}
-
-export default App;
+export default App
---
+++ src/Widget.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import OffCanvasPopup from "./Popup/OffCanvasPopup.jsx";
+import VerfiedContainer from "./VerfiedContainer/index.jsx";
+import { badgeDetails } from './helper.mjs';  
+import { useDispatch } from 'react-redux';
+import { setAllBadges, setBrandUrl } from './redux/drawerSlice.mjs';
+
+function Widget() {
+  const dispatch = useDispatch();
+  const [url, setUrl] = useState('');
+  const [badges, setBadges] = useState([]);
+
+  useEffect(() => {
+    const ecowiserVerified = document.querySelector("#ecowiser-verified");
+    const url = ecowiserVerified.dataset.url;
+    let tags = ecowiserVerified.dataset.badges.replace("Low Carbon", "Offsets Carbon").split(', ');
+    tags = tags.map((el, _) => { return el.toLowerCase(); });
+    let badges = [];
+    badgeDetails.forEach((category, _) => {
+      category.badges.forEach((badge, _) => {
+        if(tags.includes(badge.name.toLowerCase())) badges.push(badge);
+      })
+    });
+
+    setUrl(url);
+    setBadges(badges);
+    dispatch(setAllBadges(badges));
+    dispatch(setBrandUrl(url));
+  }, []);
+
+  return (
+    <>
+      <OffCanvasPopup />
+      <VerfiedContainer tags={badges} url={url} />
+    </>
+  )
+}
+
+const SampleWrapper = ({children}) => {
+  return (
+    <div className={"relative rubik w-screen h-screen flex flex-col items-center justify-center"}>
+      <div className={"relative mobile:w-[95%] w-[28rem] h-fit grid place-items-center"}>
+          {children}
+      </div>
+    </div>
+  )
+}
+
+export default Widget;
